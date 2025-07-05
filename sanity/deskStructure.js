fc2683f7b2f1fe86024e69d7cf841b81d9a3575b
Theodore Miller <71616511+theshabobo@users.noreply.github.com>
2025-07-05 10:02:29 -0500

    Update deskStructure.js

--- sanity/deskStructure.js
+++ sanity/deskStructure.js
@@ -2,6 +2,15 @@ export default (S) =>
   S.list()
     .title('Admin Dashboard')
     .items([
-      
+       // NavBar (singleton)
+      S.listItem()
+        .title('NavBar')
+        .id('navbar')
+        .child(
+          S.editor()
+            .id('navbarEditor')
+            .schemaType('navbar')
+            .documentId('navbar')
+        ),
       
     ]);
