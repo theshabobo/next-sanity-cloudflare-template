c07c5a04be8658bc63d7e965689ecd071326b327
Theodore Miller <71616511+theshabobo@users.noreply.github.com>
2025-07-05 10:04:10 -0500

    Update index.js

--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -1,26 +1,5 @@
-export default {
-  name: 'navbar',
-  title: 'Navbar',
-  type: 'document',
-  fields: [
-    {
-      name: 'siteTitle',
-      title: 'Site Title',
-      type: 'string',
-    },
-    {
-      name: 'navLinks',
-      title: 'Navigation Links',
-      type: 'array',
-      of: [
-        {
-          type: 'object',
-          fields: [
-            { name: 'label', title: 'Label', type: 'string' },
-            { name: 'href', title: 'Link URL', type: 'string' },
-          ]
-        }
-      ]
-    }
-  ]
-}
+import navbar from './navbar';
+
+export const schemaTypes = [
+    navbar,
+];
